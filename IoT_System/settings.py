473d8f1b6da59eb6c27da05f13fd2570383daadc
520h0451 <520h0451@student.tdtu.edu.vn>
2024-11-09 20:56:48 +0700

    Update new database

--- IoT_System/settings.py
+++ IoT_System/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'IoT_System.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'postgres',
+        'USER': 'postgres.tcublmtsuwjvcrjiqzgt',
+        'PASSWORD': 'Cuong_112002',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
